39429da52f534336bbc517e0f7d1cb03e7a030d7
Jorich <35774231+Yrkki@users.noreply.github.com>
2025-08-23 20:03:51 +0300

    feat(shim): pin Malwarebytes

--- maintenance/maintenance.bat
+++ maintenance/maintenance.bat
@@ -29,8 +29,10 @@ powershell -Command refreshenv
 choco outdated
 echo.
 
+@REM powershell -executionpolicy remotesigned -File .\malwarebytes.ps1
 echo Running the Windows package manager...
 echo ------------------------------------------------------
+winget pin add --id Malwarebytes.Malwarebytes
 winget upgrade --include-unknown --all
 echo.
 powershell -Command refreshenv
