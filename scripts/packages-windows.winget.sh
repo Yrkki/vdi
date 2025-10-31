2c0e5b1821257c63e16fa435b2671da7e8765822
Jorich <35774231+Yrkki@users.noreply.github.com>
2025-10-31 20:27:25 +0200

    feat(winget): add the --verbose argument

--- scripts/packages-windows.winget.sh
+++ scripts/packages-windows.winget.sh
@@ -34,169 +34,169 @@ choco feature enable -n allowGlobalConfirmation
 echo
 
 echo === Installing Storage... ====================================================================
-#winget "$verb" -e --id Dropbox.Dropbox
-winget "$verb" -e --id Google.Drive
-winget "$verb" -e --id Iterate.Cyberduck
-winget "$verb" -e --id 7zip.7zip
-winget "$verb" -e --name "Hard Disk Sentinel Professional"
-winget "$verb" -e --id MiniTool.PartitionWizard.Free
-winget "$verb" -e --id AOMEI.Backupper
-winget "$verb" -e --id AOMEI.PartitionAssistant
+#winget "$verb" --verbose -e --id Dropbox.Dropbox
+winget "$verb" --verbose -e --id Google.Drive
+winget "$verb" --verbose -e --id Iterate.Cyberduck
+winget "$verb" --verbose -e --id 7zip.7zip
+winget "$verb" --verbose -e --name "Hard Disk Sentinel Professional"
+winget "$verb" --verbose -e --id MiniTool.PartitionWizard.Free
+winget "$verb" --verbose -e --id AOMEI.Backupper
+winget "$verb" --verbose -e --id AOMEI.PartitionAssistant
 echo
 
 echo === Installing Networking... ====================================================================
-winget "$verb" -e --id Insecure.Nmap
-winget "$verb" -e --id angryziber.AngryIPScanner
+winget "$verb" --verbose -e --id Insecure.Nmap
+winget "$verb" --verbose -e --id angryziber.AngryIPScanner
 echo
 
 echo === Compute... ====================================================================
-winget "$verb" -e --id UCBerkeley.BOINC
+winget "$verb" --verbose -e --id UCBerkeley.BOINC
 echo
 
 echo === Installing OS... ====================================================================
-winget "$verb" -e --name "Alpine WSL"
-winget "$verb" -e --id Canonical.Ubuntu.2204
-winget "$verb" -e --id kalilinux.kalilinux
+winget "$verb" --verbose -e --name "Alpine WSL"
+winget "$verb" --verbose -e --id Canonical.Ubuntu.2204
+winget "$verb" --verbose -e --id kalilinux.kalilinux
 echo
 
 echo === Installing Platform... ====================================================================
-winget "$verb" -e --id OpenJS.NodeJS.LTS
+winget "$verb" --verbose -e --id OpenJS.NodeJS.LTS
 # winget upgrade -e --id Heroku.HerokuCLI --include-unknown
-winget "$verb" -e --id Heroku.HerokuCLI
-winget "$verb" -e --id Hashicorp.Terraform
-#winget "$verb" -e --id python
-winget "$verb" -e --id Docker.DockerDesktop
-#winget "$verb" -e --id docker-cli
-#winget "$verb" -e --id docker-machine
-#winget "$verb" -e --id cmake.install
-##winget "$verb" -e --id cmake
-#winget "$verb" -e --id awssamcli
-#winget "$verb" -e --id vcredist2010
-#winget "$verb" -e --id imagemagick
-winget "$verb" -e --id ImageMagick.ImageMagick
-winget "$verb" -e --id OliverBetz.ExifTool
-#winget "$verb" -e --id prometheus
+winget "$verb" --verbose -e --id Heroku.HerokuCLI
+winget "$verb" --verbose -e --id Hashicorp.Terraform
+#winget "$verb" --verbose -e --id python
+winget "$verb" --verbose -e --id Docker.DockerDesktop
+#winget "$verb" --verbose -e --id docker-cli
+#winget "$verb" --verbose -e --id docker-machine
+#winget "$verb" --verbose -e --id cmake.install
+##winget "$verb" --verbose -e --id cmake
+#winget "$verb" --verbose -e --id awssamcli
+#winget "$verb" --verbose -e --id vcredist2010
+#winget "$verb" --verbose -e --id imagemagick
+winget "$verb" --verbose -e --id ImageMagick.ImageMagick
+winget "$verb" --verbose -e --id OliverBetz.ExifTool
+#winget "$verb" --verbose -e --id prometheus
 echo
 
 echo === Installing Browser... ====================================================================
-winget "$verb" -e --id Google.Chrome
-winget "$verb" -e --id Adobe.Acrobat.Reader.64-bit
-#winget "$verb" -e --id firefox
+winget "$verb" --verbose -e --id Google.Chrome
+winget "$verb" --verbose -e --id Adobe.Acrobat.Reader.64-bit
+#winget "$verb" --verbose -e --id firefox
 echo
 
 echo === Installing Dev Tools... ====================================================================
-winget "$verb" -e --id Microsoft.WindowsTerminal.Preview
-winget "$verb" -e --id Git.Git
-winget "$verb" -e --id GitHub.GitHubDesktop
-winget "$verb" -e --id Microsoft.PowerShell.Preview
-winget "$verb" -e --id Microsoft.VisualStudioCode.Insiders
-winget "$verb" -e --id Microsoft.VisualStudioCode
+winget "$verb" --verbose -e --id Microsoft.WindowsTerminal.Preview
+winget "$verb" --verbose -e --id Git.Git
+winget "$verb" --verbose -e --id GitHub.GitHubDesktop
+winget "$verb" --verbose -e --id Microsoft.PowerShell.Preview
+winget "$verb" --verbose -e --id Microsoft.VisualStudioCode.Insiders
+winget "$verb" --verbose -e --id Microsoft.VisualStudioCode
 echo
 
 echo === Installing Provisioning Tools... ====================================================================
-winget "$verb" -e --id Hashicorp.Terraform
+winget "$verb" --verbose -e --id Hashicorp.Terraform
 echo
 
 echo === Installing R... ====================================================================
-winget "$verb" -e --id RProject.R
-winget "$verb" -e --id RProject.Rtools
-winget "$verb" -e --id Posit.RStudio
+winget "$verb" --verbose -e --id RProject.R
+winget "$verb" --verbose -e --id RProject.Rtools
+winget "$verb" --verbose -e --id Posit.RStudio
 echo
 
 echo === Installing Productivity... ====================================================================
-winget "$verb" -e --id Microsoft.Sysinternals.ProcessExplorer
-#winget "$verb" -e --id dotnetcore
-winget "$verb" -e --id Microsoft.PowerToys
-#winget "$verb" -e --id diffmerge
-winget "$verb" -e --id WinMerge.WinMerge
-winget "$verb" -e --id PuTTY.PuTTY
-winget "$verb" -e --id CPUID.CPU-Z
-winget "$verb" -e --id TechPowerUp.GPU-Z
+winget "$verb" --verbose -e --id Microsoft.Sysinternals.ProcessExplorer
+#winget "$verb" --verbose -e --id dotnetcore
+winget "$verb" --verbose -e --id Microsoft.PowerToys
+#winget "$verb" --verbose -e --id diffmerge
+winget "$verb" --verbose -e --id WinMerge.WinMerge
+winget "$verb" --verbose -e --id PuTTY.PuTTY
+winget "$verb" --verbose -e --id CPUID.CPU-Z
+winget "$verb" --verbose -e --id TechPowerUp.GPU-Z
 echo
 
 echo === Installing Viewer... ====================================================================
-winget "$verb" -e --name IrfanView64
-# winget "$verb" -e --id IrfanSkiljan.IrfanView
-# #winget "$verb" -e --id irfanview-shellextension
-# #winget "$verb" -e --id irfanviewplugins
+winget "$verb" --verbose -e --name IrfanView64
+# winget "$verb" --verbose -e --id IrfanSkiljan.IrfanView
+# #winget "$verb" --verbose -e --id irfanview-shellextension
+# #winget "$verb" --verbose -e --id irfanviewplugins
 echo
 
 echo === Graphics... ====================================================================
-winget "$verb" -e --id GIMP.GIMP
-winget "$verb" -e --id BlenderFoundation.Blender
+winget "$verb" --verbose -e --id GIMP.GIMP
+winget "$verb" --verbose -e --id BlenderFoundation.Blender
 echo
 
 echo === AWS... ====================================================================
-winget "$verb" -e --id Amazon.AWSCLI
-winget "$verb" -e --id Amazon.SAM-CLI
-winget "$verb" -e --id Amazon.SessionManagerPlugin
-winget "$verb" -e --id Amazon.AWSVPNClient
-winget "$verb" -e --id ChefSoftware.Workstation
-winget "$verb" -e --id Serverless.Serverless
+winget "$verb" --verbose -e --id Amazon.AWSCLI
+winget "$verb" --verbose -e --id Amazon.SAM-CLI
+winget "$verb" --verbose -e --id Amazon.SessionManagerPlugin
+winget "$verb" --verbose -e --id Amazon.AWSVPNClient
+winget "$verb" --verbose -e --id ChefSoftware.Workstation
+winget "$verb" --verbose -e --id Serverless.Serverless
 echo
 
 echo === Hardware Tools... ====================================================================
-winget "$verb" -e --id ALCPU.CoreTemp
+winget "$verb" --verbose -e --id ALCPU.CoreTemp
 echo
 
 echo === Installing Dell Drivers... ====================================================================
-winget "$verb" -e --id Dell.CommandUpdate.Universal
-winget "$verb" -e --id Dell.CommandConfigure
-winget "$verb" -e --id Dell.DisplayManager
-winget "$verb" -e --name "Dell Digital Delivery"
-winget "$verb" -e --name "My Dell"
-winget "$verb" -e --name "Media Suite Essentials for Dell"
+winget "$verb" --verbose -e --id Dell.CommandUpdate.Universal
+winget "$verb" --verbose -e --id Dell.CommandConfigure
+winget "$verb" --verbose -e --id Dell.DisplayManager
+winget "$verb" --verbose -e --name "Dell Digital Delivery"
+winget "$verb" --verbose -e --name "My Dell"
+winget "$verb" --verbose -e --name "Media Suite Essentials for Dell"
 echo
 
 echo === Installing Intel Drivers... ====================================================================
-winget "$verb" -e --id Intel.IntelDriverAndSupportAssistant
-winget "$verb" -e --id Solidigm.StorageTool
+winget "$verb" --verbose -e --id Intel.IntelDriverAndSupportAssistant
+winget "$verb" --verbose -e --id Solidigm.StorageTool
 echo
 
 echo === Installing Logitech Drivers... ====================================================================
-winget "$verb" -e --id Logitech.SetPoint
-winget "$verb" -e --id Logitech.LGS
-# winget "$verb" -e --id Logitech.GHUB
-# winget "$verb" -e --id Logitech.LogiTune
-# winget "$verb" -e --id Logitech.CameraSettings
+winget "$verb" --verbose -e --id Logitech.SetPoint
+winget "$verb" --verbose -e --id Logitech.LGS
+# winget "$verb" --verbose -e --id Logitech.GHUB
+# winget "$verb" --verbose -e --id Logitech.LogiTune
+# winget "$verb" --verbose -e --id Logitech.CameraSettings
 echo
 
 echo === Benchmarking... ====================================================================
-winget "$verb" -e --id FinalWire.AIDA64.Extreme
+winget "$verb" --verbose -e --id FinalWire.AIDA64.Extreme
 echo
 
 echo === Personalization... ====================================================================
-# winget "$verb" -e --id 9PF4KZ2VN4W9  # TranslucentTB
+# winget "$verb" --verbose -e --id 9PF4KZ2VN4W9  # TranslucentTB
 echo
 
 echo === Office... ====================================================================
-winget "$verb" -e --id Adobe.Acrobat.Reader.64-bit
+winget "$verb" --verbose -e --id Adobe.Acrobat.Reader.64-bit
 echo
 
 echo === Media... ====================================================================
-winget "$verb" -e --id Spotify.Spotify
-winget "$verb" -e --id Daum.PotPlayer
-winget "$verb" -e --id OBSProject.OBSStudio
+winget "$verb" --verbose -e --id Spotify.Spotify
+winget "$verb" --verbose -e --id Daum.PotPlayer
+winget "$verb" --verbose -e --id OBSProject.OBSStudio
 echo
 
 echo === Conferencing... ====================================================================
-# winget "$verb" -e --id Cisco.CiscoWebexMeetings
-# winget "$verb" -e --id SlackTechnologies.Slack
+# winget "$verb" --verbose -e --id Cisco.CiscoWebexMeetings
+# winget "$verb" --verbose -e --id SlackTechnologies.Slack
 echo
 
 echo === Security... ====================================================================
-# winget "$verb" -e --id Malwarebytes.Malwarebytes
-# winget "$verb" -e --id Microsoft.Sysinternals.Autoruns
-winget "$verb" -e --id Microsoft.Sysinternals.Suite
+# winget "$verb" --verbose -e --id Malwarebytes.Malwarebytes
+# winget "$verb" --verbose -e --id Microsoft.Sysinternals.Autoruns
+winget "$verb" --verbose -e --id Microsoft.Sysinternals.Suite
 echo
 
 echo === Miscellaneous Tools... ====================================================================
-# winget "$verb" -e --id 
+# winget "$verb" --verbose -e --id ...
 echo
 
 echo === Upgrade all... ====================================================================
 echo Upgrade all...
-winget upgrade --all
+winget upgrade --include-unknown --all
 echo
 
 # echo === Refreshing OS Environment... ====================================================================
